e363d37013fdbb864960e3aecded2573a3a9a08f
ttrider <software@ttrider.com>
2015-04-21 23:07:31 -0700

    refactoring

---
+++ VSExtensionsImportExport/ExtensionInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace TTRider.ExportExtensions
+{
+    internal class ExtensionInfo
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Identifier { get; set; }
+        public string DownloadUrl { get; set; }
+        public string DownloadAs { get; set; }
+    }
+}
